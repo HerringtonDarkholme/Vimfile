af96eb0dfc90fc9ed457abefa92b5d9e0dda6f29
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2021-10-04 00:02:48 +0800

    update nvim

--- .nvimrc
+++ .nvimrc
@@ -177,7 +177,7 @@ nn <A-a> :bp<CR>
 nn <A-d> :bn<CR>
 
 
-tnoremap <esc><esc> <C-\><C-n>
+" tnoremap <esc><esc> <C-\><C-n>
 
 """"""""""""""""""""""""""""""""""""""""""""""""
 " => Editing Mappings!!
@@ -209,7 +209,7 @@ autocmd FileType cpp nn <localleader>h :vert sf %:t:r.h<cr>
 " set <A-a>=a
 
 " => File Browswing
-nn - :NERDTreeToggle<CR>
+nn - :call g:setup_nvim_tree()<CR>:NvimTreeToggle<CR>
 let NERDTreeMapOpenSplit = 's'
 let NERDTreeMapOpenVSplit = 'v'
 let NERDTreeIgnore = ['\.pyc$']
@@ -222,10 +222,10 @@ nnoremap <silent><F2> :call ToggleLocationList()<CR>
 nn <silent> <F8> :TagbarToggle<CR>
 
 "CtrlP like"
-nn <C-p> :<C-u>Files<cr>
+nn <C-p> :<C-u>FzfLua files<cr>
 
 "undo list"
-nn <silent> <leader>u :GundoToggle<CR>
+" nn <silent> <leader>u :GundoToggle<CR>
 
 "emmet
 " autocmd BufEnter *.xml imap <buffer><expr> <tab> emmet#expandAbbrIntelligent("\<tab>")
@@ -244,7 +244,7 @@ function! s:search_word()
   let ag_opt = '-s --hidden --' . expand('%:e')
   call fzf#vim#ag(expand('<cword>'), ag_opt, arg, v:false)
 endfunction
-nn <silent> <leader>g :Ag<Space>
+nn <silent> <leader>g :FzfLua grep<CR>
 nn <silent> gr :call <SID>search_word()<CR>
 
 "let g:dein#install_process_timeout=1000
@@ -301,7 +301,8 @@ let g:fzf_nvim_statusline = 0 " disable statusline overwriting
 let g:fzf_layout = {'down': '15'}
 
 set rtp+=~/.vim/dein/repos/github.com/Shougo/dein.vim/
-set rtp+=~/.fzf
+" set rtp+=~/.fzf
+
 
 command! -nargs=+ -buffer Dein call dein#add(<args>)
 
@@ -315,7 +316,7 @@ Dein 'shadmansaleh/lualine.nvim'
 " Dein 'vim-airline/vim-airline-themes'
 " Dein 'bling/vim-bufferline'
 Dein 'Raimondi/delimitMate'
-Dein 'HerringtonDarkholme/vim-nerdtree-syntax-highlight'
+" Dein 'HerringtonDarkholme/vim-nerdtree-syntax-highlight'
 " Dein 'Shougo/vimproc.vim', {'build': 'make'}
 Dein 'airblade/vim-gitgutter'
 Dein 'michaeljsmith/vim-indent-object'
@@ -325,25 +326,31 @@ Dein 'tpope/vim-commentary'
 Dein 'tpope/vim-surround'
 Dein 'lifepillar/vim-solarized8'
 " Dein 'altercation/vim-colors-solarized'
-Dein 'ryanoasis/vim-devicons'
+" Dein 'ryanoasis/vim-devicons'
 Dein 'editorconfig/editorconfig-vim'
-Dein 'junegunn/fzf'
-Dein 'junegunn/fzf.vim', {'depends': 'fzf'}
+
+Dein 'vijaymarupudi/nvim-fzf'
+Dein 'ibhagwan/fzf-lua', {'on_idle': 1, 'hook_source': 'call g:setup_fzf_lua()'}
+Dein 'kyazdani42/nvim-web-devicons'
+Dein 'kyazdani42/nvim-tree.lua'
+
+" Dein 'junegunn/fzf'
+" Dein 'junegunn/fzf.vim', {'depends': 'fzf'}
 Dein 'folke/which-key.nvim'
 
 Dein 'justmao945/vim-clang', {'on_ft': ['c', 'cpp']}
 Dein 'octol/vim-cpp-enhanced-highlight', {'on_ft': 'cpp'}
-Dein 'derekwyatt/vim-scala', {'on_ft': 'scala'}
+" Dein 'derekwyatt/vim-scala', {'on_ft': 'scala'}
 Dein 'rust-lang/rust.vim', {'on_ft': 'rust'}
 Dein 'fatih/vim-go', {'on_ft': 'go'}
-Dein 'digitaltoad/vim-pug', {'on_ft': ['pug', 'vue']}
+" Dein 'digitaltoad/vim-pug', {'on_ft': ['pug', 'vue']}
 Dein 'posva/vim-vue', {'on_ft': ['vue']}
-Dein 'mattn/emmet-vim', {'on_ft': ['html', 'xml', 'vue']}
+" Dein 'mattn/emmet-vim', {'on_ft': ['html', 'xml', 'vue']}
 Dein 'othree/html5.vim', {'on_ft': ['html', 'pug']}
-Dein 'wavded/vim-stylus', {'on_ft': ['stylus', 'vue']}
+" Dein 'wavded/vim-stylus', {'on_ft': ['stylus', 'vue']}
 Dein 'cakebaker/scss-syntax.vim', {'on_ft': ['scss', 'sass']}
 Dein 'maksimr/vim-jsbeautify', {'on_ft': 'javascript'}
-Dein 'flowtype/vim-flow', {'on_ft': 'javascript'}
+" Dein 'flowtype/vim-flow', {'on_ft': 'javascript'}
 Dein 'othree/yajs.vim', {'on_ft': 'javascript'}
 Dein 'mxw/vim-jsx', {'on_ft': 'javascript'}
 Dein 'othree/es.next.syntax.vim', {'on_ft': 'javascript'}
@@ -353,9 +360,9 @@ Dein 'solarnz/thrift.vim', {'on_ft': ['thrift']}
 
 Dein 'tpope/vim-fugitive', {'on_cmd': ['Git']}
 Dein 'godlygeek/tabular',  {'on_cmd': 'Tabularize'}
-Dein 'majutsushi/tagbar',  {'on_cmd': 'TagbarToggle'}
+" Dein 'majutsushi/tagbar',  {'on_cmd': 'TagbarToggle'}
 Dein 'scrooloose/nerdtree',  {'on_cmd': 'NERDTreeToggle'}
-Dein 'sjl/gundo.vim',  {'on_cmd': 'GundoToggle'}
+" Dein 'sjl/gundo.vim',  {'on_cmd': 'GundoToggle'}
 Dein 'Shougo/vinarise.vim',  {'on_cmd': 'Vinarise'}
 
 Dein 'honza/vim-snippets', {'on_i': 1}
@@ -368,27 +375,11 @@ Dein 'kamykn/spelunker.vim', {'on_i': 1}
 Dein 'milkypostman/vim-togglelist', {'on_func': 'ToggleLocationList'}
 Dein 'othree/eregex.vim', {'on_func': 'eregex#toggle'}
 
+exec 'source ' . expand('<sfile>:p:h') . '/nvim.lua'
+
 " Dein 'gerw/vim-HiLinkTrace'
-
-lua << EOF
-  require('lualine').setup {
-    options = {theme = 'solarized_light'},
-    tabline = {
-      lualine_b = {'buffers'},
-      lualine_z = {'tabs'}
-    },
-    extensions = {'fzf', 'nerdtree'},
-  }
-  require('which-key').setup {
-    plugins = {
-      spelling = {
-        enabled = true,
-      },
-    }
-  }
-EOF
-
 call dein#end()
+
 
 " call coc#add_extension('coc-json', 'coc-tsserver', 'coc-css', 'coc-html', 'coc-vetur', 'coc-pyls', 'coc-rls')
 
---
+++ nvim.lua
@@ -0,0 +1,193 @@
+require('lualine').setup {
+  options = {theme = 'solarized_light'},
+  tabline = {
+    lualine_b = {'buffers'},
+    lualine_z = {'tabs'}
+  },
+  extensions = {'fzf', 'nerdtree'},
+}
+require('which-key').setup {
+  plugins = {
+    spelling = {
+      enabled = true,
+    },
+  }
+}
+local fzf_loaded = false
+vim.g.setup_fzf_lua = function()
+  if fzf_loaded then return end
+  fzf_loaded = true
+
+  local actions = require "fzf-lua.actions"
+  require'fzf-lua'.setup {
+    winopts = {
+      split         = "belowright new",-- open in a split instead?
+    },
+    keymap = {
+      -- These override the default tables completely
+      -- no need to set to `false` to disable a bind
+      -- delete or modify is sufficient
+      builtin = {
+        -- neovim `:tmap` mappings for the fzf win
+        ["<F2>"]        = "toggle-fullscreen",
+        -- Only valid with the 'builtin' previewer
+        ["<F3>"]        = "toggle-preview-wrap",
+        ["<F4>"]        = "toggle-preview",
+        ["<S-down>"]    = "preview-page-down",
+        ["<S-up>"]      = "preview-page-up",
+        ["<S-left>"]    = "preview-page-reset",
+      },
+      fzf = {
+        -- fzf '--bind=' options
+        ["ctrl-u"]      = "unix-line-discard",
+        ["ctrl-f"]      = "half-page-down",
+        ["ctrl-b"]      = "half-page-up",
+        ["ctrl-a"]      = "beginning-of-line",
+        ["ctrl-e"]      = "end-of-line",
+        ["alt-a"]       = "toggle-all",
+        -- Only valid with fzf previewers (bat/cat/git/etc)
+        ["f3"]          = "toggle-preview-wrap",
+        ["f4"]          = "toggle-preview",
+        ["shift-down"]  = "preview-page-down",
+        ["shift-up"]    = "preview-page-up",
+      },
+    },
+    -- use skim instead of fzf?
+    -- https://github.com/lotabout/skim
+    -- fzf_bin          = 'sk',
+    fzf_opts = {
+      -- options are sent as `<left>=<right>`
+      -- set to `false` to remove a flag
+      -- set to '' for a non-value flag
+      -- for raw args use `fzf_args` instead
+      ['--ansi']        = '',
+      ['--prompt']      = ' >',
+      ['--info']        = 'inline',
+      ['--height']      = '100%',
+      ['--layout'] = 'default',
+    },
+    preview_border      = 'border',       -- border|noborder
+    preview_wrap        = 'nowrap',       -- wrap|nowrap
+    preview_opts        = 'nohidden',     -- hidden|nohidden
+    preview_vertical    = 'down:45%',     -- up|down:size
+    preview_horizontal  = 'right:50%',    -- right|left:size
+    preview_layout      = 'horizontal',         -- horizontal|vertical|flex
+    flip_columns        = 120,            -- #cols to switch to horizontal on flex
+    default_previewer   = "bat",       -- override the default previewer?
+    -- by default uses the builtin previewer
+    previewers = {
+      bat = {
+        cmd             = "bat",
+        args            = "--style=numbers,changes --color always",
+        config          = nil,            -- nil uses $BAT_CONFIG_PATH
+      },
+      head = {
+        cmd             = "head",
+        args            = nil,
+      },
+      git_diff = {
+        cmd             = "git diff",
+        args            = "--color",
+      },
+    },
+    -- provider setup
+    files = {
+      -- previewer         = "cat",       -- uncomment to override previewer
+      prompt            = 'Files❯ ',
+      cmd               = 'ag -g ""',             -- "find . -type f -printf '%P\n'",
+      git_icons         = false,           -- show git icons?
+      file_icons        = true,           -- show file icons?
+      color_icons       = true,           -- colorize file|git icons
+    },
+    grep = {
+      prompt            = 'Ag❯ ',
+      input_prompt      = 'Grep For❯ ',
+      cmd               = "ag --color",
+      git_icons         = false,           -- show git icons?
+      file_icons        = true,           -- show file icons?
+      color_icons       = true,           -- colorize file|git icons
+    },
+    oldfiles = {
+      prompt            = 'History❯ ',
+      cwd_only          = false,
+    },
+    -- uncomment to disable the previewer
+    -- nvim = { marks    = { previewer = { _ctor = false } } },
+    helptags = { previewer = { _ctor = false } },
+    -- manpages = { previewer = { _ctor = false } },
+    -- uncomment to set dummy win location (help|man bar)
+    -- "topleft"  : up
+    -- "botright" : down
+    -- helptags = { previewer = { split = "topleft" } },
+    -- manpages = { previewer = { split = "topleft" } },
+    -- uncomment to use `man` command as native fzf previewer
+    -- manpages = { previewer = { _ctor = require'fzf-lua.previewer'.fzf.man_pages } },
+    -- optional override of file extension icon colors
+    -- available colors (terminal):
+    --    clear, bold, black, red, green, yellow
+    --    blue, magenta, cyan, grey, dark_grey, white
+    -- padding can help kitty term users with
+    -- double-width icon rendering
+    file_icon_padding = '',
+  }
+end
+
+local tree_loaded = false
+vim.g.setup_nvim_tree = function()
+  if tree_loaded then return end
+  tree_loaded = true
+
+  vim.g.nvim_tree_ignore = { '.git', 'node_modules', '.cache' } -- empty by default
+  vim.g.nvim_tree_gitignore = 1
+  vim.g.nvim_tree_indent_markers = 1
+  vim.g.nvim_tree_hide_dotfiles = 1
+  vim.g.nvim_tree_group_empty = 1
+  -- vim.g.nvim_tree_disable_window_picker = 1 "0 by default, will disable the window picker.
+  vim.g.nvim_tree_icon_padding = ' '
+
+  local tree_cb = require'nvim-tree.config'.nvim_tree_callback
+  -- default mappings
+  local list = {
+    { key = {"<CR>", "o", "<2-LeftMouse>"}, cb = tree_cb("edit") },
+    { key = "v",                            cb = tree_cb("vsplit") },
+    { key = "C",                            cb = tree_cb("cd") },
+    { key = "s",                            cb = tree_cb("split") },
+    { key = "t",                            cb = tree_cb("tabnew") },
+    { key = "<",                            cb = tree_cb("prev_sibling") },
+    { key = ">",                            cb = tree_cb("next_sibling") },
+    { key = "<Tab>",                        cb = tree_cb("preview") },
+    { key = "K",                            cb = tree_cb("first_sibling") },
+    { key = "J",                            cb = tree_cb("last_sibling") },
+    { key = "H",                            cb = tree_cb("toggle_ignored") },
+    { key = "I",                            cb = tree_cb("toggle_dotfiles") },
+    { key = "R",                            cb = tree_cb("refresh") },
+    { key = "a",                            cb = tree_cb("create") },
+    { key = "d",                            cb = tree_cb("remove") },
+    { key = "r",                            cb = tree_cb("rename") },
+    { key = "<C-r>",                        cb = tree_cb("full_rename") },
+    { key = "x",                            cb = tree_cb("close_node") },
+    { key = "yy",                           cb = tree_cb("copy") },
+    { key = "P",                            cb = tree_cb("paste") },
+    { key = "<c-y>",                        cb = tree_cb("copy_name") },
+    { key = "Y",                            cb = tree_cb("copy_path") },
+    { key = "gy",                           cb = tree_cb("copy_absolute_path") },
+    { key = "[c",                           cb = tree_cb("prev_git_item") },
+    { key = "]c",                           cb = tree_cb("next_git_item") },
+    { key = "<BC>",                         cb = tree_cb("dir_up") },
+    { key = "<c-s>",                        cb = tree_cb("system_open") },
+    { key = "q",                            cb = tree_cb("close") },
+    { key = "g?",                           cb = tree_cb("toggle_help") },
+  }
+  require'nvim-tree'.setup {
+    view = {
+      width = 35,
+      mappings = {
+        -- custom only false will merge the list with the default mappings
+        -- if true, it will only use your list to set the mappings
+        custom_only = true,
+        -- list of mappings to set on the tree manually
+        list = list
+      }
+    }
+  }
+end

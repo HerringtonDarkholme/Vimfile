149d797564ae11f9f8c35f990913f2d4311e468e
HerrintonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2024-03-05 16:55:36 -0800

    chore: update config

--- nvim.lua
+++ nvim.lua
@@ -348,7 +348,7 @@ vim.g.setup_nvim_cmp = function()
   -- })
 
   -- Setup lspconfig.
-  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
   -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 
   -- keybindings
